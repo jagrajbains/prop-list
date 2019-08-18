fbada210de1036176075ce31f5e0b87f2dba6881
Jagraj Bains <jagrajbains.94@gmail.com>
2019-08-18 12:38:18 +0530

    adding new file

---
+++ src/firebase/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyC-DpAiKwifoqTj9f_QfFrPiJVMgJV-ZVk",
+  authDomain: "prop-list.firebaseapp.com",
+  databaseURL: "https://prop-list.firebaseio.com",
+  projectId: "prop-list",
+  storageBucket: "gs://prop-list.appspot.com/",
+  messagingSenderId: "48554466107",
+  appId: "1:48554466107:web:b9469cb59e9f14fb"
+};
